0204fdc9d27904922de6ae15d550c057018cc32f
sajid-islam <sajidislam941@gmail.com>
2025-08-16 02:17:34 +0600

    remove authSlice.jsx

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import navbarReducer from "./navbar/NavbarSlice";
-import authReducer from "./auth/authSlice";
 
 export const store = configureStore({
     reducer: {
         navbar: navbarReducer,
-        auth: authReducer,
     },
 });
