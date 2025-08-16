bd2ec86e5c6fd5763b5338bb2b815c6aae4bb29b
sajid-islam <sajidislam941@gmail.com>
2025-08-16 13:45:48 +0600

    Implement axios interceptors

--- src/hooks/useAxiosPrivate.jsx
+++ src/hooks/useAxiosPrivate.jsx
@@ -1,10 +1,40 @@
 import axios from "axios";
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useRouter } from "next/navigation";
 
 const AxiosPrivate = axios.create({
     baseURL: "http://localhost:3001",
     withCredentials: true,
 });
+
 const useAxiosPrivate = () => {
+    const { logout } = useAuth();
+    const router = useRouter();
+
+    useEffect(() => {
+        // Use the instance for interceptor
+        const responseInterceptor = AxiosPrivate.interceptors.response.use(
+            (res) => res,
+            (error) => {
+                if (
+                    error.response?.status === 401 ||
+                    error.response?.status === 403
+                ) {
+                    logout().then(() => {
+                        router.push("/sign-in");
+                    });
+                }
+                return Promise.reject(error);
+            }
+        );
+
+        // Cleanup interceptor on unmount
+        return () => {
+            AxiosPrivate.interceptors.response.eject(responseInterceptor);
+        };
+    }, [logout, router]);
+
     return AxiosPrivate;
 };
 
