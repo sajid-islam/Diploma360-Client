777870bce87587089614468b93eda24dc40f1b5e
sajid-islam <sajidislam941@gmail.com>
2025-08-17 19:04:12 +0600

    remove reload state

--- src/hooks/useIsAdmin.jsx
+++ src/hooks/useIsAdmin.jsx
@@ -5,28 +5,25 @@ import useAuth from "./useAuth";
 
 const useIsAdmin = () => {
     const [isAdmin, setIsAdmin] = useState(false);
-    const [reload, setReload] = useState(false);
     const { loading, user } = useAuth();
+    console.log(isAdmin);
     const AxiosPrivate = useAxiosPrivate();
 
     useEffect(() => {
-        if (loading || !user) return; // wait until user is ready
-
+        if (loading || !user) return;
         const checkAdmin = async () => {
             try {
                 const res = await AxiosPrivate.get("/api/user/is-admin");
+
                 setIsAdmin(res.data.isAdmin);
             } catch (error) {
-                if (error.response?.status === 401) {
-                    setReload(true);
-                }
                 console.error("Admin check failed:", error);
                 setIsAdmin(false);
             }
         };
 
         checkAdmin();
-    }, [loading, user, AxiosPrivate, reload]); // ✅ include necessary deps
+    }, [loading, AxiosPrivate, user]);
 
     return { isAdmin };
 };
