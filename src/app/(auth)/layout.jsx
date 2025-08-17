cd49af5c3df9534161b0c7a498394ae09698e6ff
sajid-islam <sajidislam941@gmail.com>
2025-08-17 10:03:29 +0600

    remove classnames from authLayout

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -6,9 +6,7 @@ import { Suspense } from "react";
 export default function AuthLayout({ children }) {
     return (
         <Suspense fallback={<div>Loading...</div>}>
-            <div className="min-h-screen flex items-center justify-center bg-gray-50">
-                {children}
-            </div>
+            <div>{children}</div>
         </Suspense>
     );
 }
