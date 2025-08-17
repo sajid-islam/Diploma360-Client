ae0699732f060c729f17f5679ffe1556447ea9c9
sajid-islam <sajidislam941@gmail.com>
2025-08-17 17:32:05 +0600

    check isAdmin after user loaded

--- src/hooks/useIsAdmin.jsx
+++ src/hooks/useIsAdmin.jsx
@@ -1,31 +1,32 @@
 "use client";
 import { useState, useEffect } from "react";
 import useAxiosPrivate from "./useAxiosPrivate";
+import useAuth from "./useAuth";
 
 const useIsAdmin = () => {
     const [isAdmin, setIsAdmin] = useState(false);
-    const [loading, setLoading] = useState(true);
+    const [loading] = useAuth();
     console.log(isAdmin);
     const AxiosPrivate = useAxiosPrivate();
 
     useEffect(() => {
         const checkAdmin = async () => {
             try {
-                const res = await AxiosPrivate.get("/api/user/is-admin");
-                const data = await res.data;
-                setIsAdmin(data.isAdmin);
+                if (!loading) {
+                    const res = await AxiosPrivate.get("/api/user/is-admin");
+                    const data = await res.data;
+                    setIsAdmin(data.isAdmin);
+                }
             } catch (error) {
                 console.error("Admin check failed:", error);
                 setIsAdmin(false);
-            } finally {
-                setLoading(false);
             }
         };
 
         checkAdmin();
     }, []);
 
-    return { isAdmin, loading };
+    return { isAdmin };
 };
 
 export default useIsAdmin;
