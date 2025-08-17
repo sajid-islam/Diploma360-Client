af94a617dfe5648a94a72d3c05a07edcf6bd3241
sajid-islam <sajidislam941@gmail.com>
2025-08-17 17:09:46 +0600

    remove axios interseptor

--- src/hooks/useAxiosPrivate.jsx
+++ src/hooks/useAxiosPrivate.jsx
@@ -1,7 +1,4 @@
 import axios from "axios";
-import { useEffect } from "react";
-import useAuth from "./useAuth";
-import { useRouter } from "next/navigation";
 
 const AxiosPrivate = axios.create({
     baseURL: "https://diploma360-server.vercel.app",
@@ -9,32 +6,6 @@ const AxiosPrivate = axios.create({
 });
 
 const useAxiosPrivate = () => {
-    const { logout } = useAuth();
-    const router = useRouter();
-
-    useEffect(() => {
-        // Use the instance for interceptor
-        const responseInterceptor = AxiosPrivate.interceptors.response.use(
-            (res) => res,
-            (error) => {
-                if (
-                    error.response?.status === 401 ||
-                    error.response?.status === 403
-                ) {
-                    logout().then(() => {
-                        router.push("/sign-in");
-                    });
-                }
-                return Promise.reject(error);
-            }
-        );
-
-        // Cleanup interceptor on unmount
-        return () => {
-            AxiosPrivate.interceptors.response.eject(responseInterceptor);
-        };
-    }, [logout, router]);
-
     return AxiosPrivate;
 };
 
